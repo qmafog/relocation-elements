13d0fad546cd6eef3fec9477aff48a5ccdf2ec19
Alexander <154324-alexander-rykov@users.noreply.gitlab.com>
2022-02-09 19:12:15 +0300

    Add solution configuration files: editor configs, stylecop settings, style ruleset, readme. Add projects with unit tests.

---
+++ RelocationElements/ArrayExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RelocationElements
+{
+    /// <summary>
+    /// Class for operations with array.
+    /// </summary>
+    public static class ArrayExtension
+    {
+        /// <summary>
+        /// Moves all of the elements with set value to the end, preserving the order of the other elements.
+        /// </summary>
+        /// <param name="source"> Source array. </param>
+        /// <param name="value">Source value.</param>
+        /// <exception cref="ArgumentNullException">Thrown when source array is null.</exception>
+        /// <exception cref="ArgumentException">Thrown when source array is empty.</exception>
+        public static void MoveToTail(int[]? source, int value)
+        {
+            throw new NotImplementedException("You need to implement this function.");
+        }
+    }
+}
